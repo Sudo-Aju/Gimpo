101247cfe0a775322536d1fd4c58a1e3a2d61435
Azmeerpirani <azmeerpirani3@gmail.com>
2026-08-24 00:53:11 +0530

    made some basic stuff, learnt a lot, starting to understand some logic

--- main.cpp
+++ main.cpp
@@ -1,17 +1,19 @@
 #include <iostream>
 
-int GivePlayerHealth()
+void PlayerStats(int damage, int health, int speed, std::string PlayerName)
 {
-    return 52;
+ std::cout << "Player Stats:" << std::endl;
+ std::cout << "Player's Name:" << PlayerName << std::endl;
+ std::cout << "Damage:" << damage << std::endl;
+ std::cout << "Health:" << health << std::endl;
+ std::cout << "Speed:" << speed << std::endl;
 }
 
-
-
 int main() 
-{
-    int hp = GivePlayerHealth();
-
-    std::cout << hp << std::endl;
+{  
+    
+    PlayerStats(1, 100, 2, "Azmeer");
+    PlayerStats(40, 90, 3, "Soup");
     return 0;
 }
 
