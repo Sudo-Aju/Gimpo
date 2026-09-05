9c7c7c0c937e693380ddf9a74c5390cd6af3e619
Azmeerpirani <azmeerpirani3@gmail.com>
2026-09-05 11:27:04 +0530

    Used preconfigured sfml apps to find how to launcha. sfml app, and added more libraries to use in main.cpp

--- main.cpp
+++ main.cpp
@@ -1,19 +1,14 @@
 #include <iostream>
+#include <ctime>
+#include <cstdlib>
 
-void PlayerStats(int damage, int health, int speed, std::string PlayerName)
+
+#include <SFML/Graphics.hpp>
+
+int main()
 {
- std::cout << "Player Stats:" << std::endl;
- std::cout << "Player's Name:" << PlayerName << std::endl;
- std::cout << "Damage:" << damage << std::endl;
- std::cout << "Health:" << health << std::endl;
- std::cout << "Speed:" << speed << std::endl;
-}
+    std::cout << "First Test!" << "\n";
 
-int main() 
-{  
-    
-    PlayerStats(1, 100, 2, "Azmeer");
-    PlayerStats(40, 90, 3, "Soup");
+    std::cin.get();
     return 0;
 }
-
