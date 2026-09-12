18fd49b22ee64aa430b988b7184eab05f60472a2
Azmeerpirani <azmeerpirani3@gmail.com>
2026-09-12 12:45:08 +0530

    Redid few stuff, tried to better understand what are constrcutors about

--- main.cpp
+++ main.cpp
@@ -1,27 +1,31 @@
 #include <iostream>
 #include <ctime>
 #include <cstdlib>
-
-
 #include <SFML/Graphics.hpp>
 
 int main()
 {
     sf::RenderWindow window(
-        sf::VideoMode({400, 1200}),
-        "it is wokring"
+        sf::VideoMode({800,600}),
+        "Gimpo"
     );
+
+    sf::CircleShape circle(50);
 
     while (window.isOpen())
     {
         while (const std::optional event = window.pollEvent())
         {
-            if (event->is<sf::Event::Closed>())
-            {
-                window.close();
-            }
+        if (event->is<sf::Event::Closed>())
+            window.close();
         }
+        
+    window.clear();
+
+    window.draw(circle);
+
+    window.display();
     }
 
-    return 0;
-}   
+    
+}
