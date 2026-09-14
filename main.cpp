86ace3ea3657c9978b9e5727abdba22a6f91d5d5
Azmeerpirani <azmeerpirani3@gmail.com>
2026-09-14 12:25:41 +0530

    Expirimented more, added few shapes and also added variables to make them move, now i know how to use sfml fucntions to get keyboard input and use the loop to make it move

--- main.cpp
+++ main.cpp
@@ -1,16 +1,27 @@
 #include <iostream>
-#include <ctime>
-#include <cstdlib>
 #include <SFML/Graphics.hpp>
+
+
 
 int main()
 {
+    int playerScore = 0;
+    int opponentScore = 0;
+    float paddleSpeed = 300.0f;
+
     sf::RenderWindow window(
         sf::VideoMode({800,600}),
         "Gimpo"
     );
 
-    sf::CircleShape circle(50);
+    float paddlePos1 = 250.0f;
+
+    sf::RectangleShape leftPaddle({20.f,100.f});
+    sf::RectangleShape rightPaddle({20.f,100.f});
+    sf::CircleShape ball(12.f);
+    ball.setPosition({388,288});
+    
+    rightPaddle.setPosition({755,250});
 
     while (window.isOpen())
     {
@@ -20,9 +31,19 @@ int main()
             window.close();
         }
         
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::W)) {
+            paddlePos1 -= 0.02;
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::S)) {
+            paddlePos1 += 0.02;
+        }
+        leftPaddle.setPosition({25,paddlePos1});
+        
     window.clear();
-
-    window.draw(circle);
+    
+    window.draw(leftPaddle);
+    window.draw(rightPaddle);
+    window.draw(ball);
 
     window.display();
     }
