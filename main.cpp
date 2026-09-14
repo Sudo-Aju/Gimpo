b813c1124715c0d49245dde55d18b63695377d76
Azmeerpirani <azmeerpirani3@gmail.com>
2026-09-14 12:49:33 +0530

    learnt about how differnt systems may run on different fps so speed might change, so took help of sf::Clock to meaasure the time in intervals of frames to multiply it with the speed. also added the limits of how much the paddle can go.

--- main.cpp
+++ main.cpp
@@ -15,29 +15,60 @@ int main()
     );
 
     float paddlePos1 = 250.0f;
+    float paddlePos2 = 250.0f;
 
     sf::RectangleShape leftPaddle({20.f,100.f});
     sf::RectangleShape rightPaddle({20.f,100.f});
     sf::CircleShape ball(12.f);
     ball.setPosition({388,288});
     
-    rightPaddle.setPosition({755,250});
+    
+
+    sf::Clock clock;
 
     while (window.isOpen())
     {
+        float deltaTime = clock.restart().asSeconds();
         while (const std::optional event = window.pollEvent())
+        
         {
         if (event->is<sf::Event::Closed>())
             window.close();
         }
         
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::W)) {
-            paddlePos1 -= 0.02;
+            paddlePos1 -= deltaTime * paddleSpeed;
         }
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::S)) {
-            paddlePos1 += 0.02;
+            paddlePos1 += deltaTime * paddleSpeed;
         }
+
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Up)) {
+            paddlePos2 -= deltaTime * paddleSpeed;
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Down)) {
+            paddlePos2 += deltaTime * paddleSpeed;
+        }
+
         leftPaddle.setPosition({25,paddlePos1});
+        rightPaddle.setPosition({755,paddlePos2});
+
+        sf::Vector2f pos1 = leftPaddle.getPosition();
+        sf::Vector2f pos2 = rightPaddle.getPosition();
+        
+        if (pos1.y < 0) {
+            paddlePos1 = 0;
+        }
+        if (pos1.y > 500) {
+            paddlePos1 = 500;
+        }
+
+        if (pos2.y < 0) {
+            paddlePos2 = 0;
+        }
+        if (pos2.y > 500) {
+            paddlePos2 = 500;
+        }
         
     window.clear();
     
